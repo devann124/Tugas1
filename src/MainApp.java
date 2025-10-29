d92a909aa7d9b503687a3230a3aa820a0c9f8c64
devann124 <devankasyafani2@gmail.com>
2025-10-29 16:43:21 +0700

    Update MainApp.java

--- src/MainApp.java
+++ src/MainApp.java
@@ -7,8 +7,8 @@ public class MainApp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        MenuItem Seblak special = new MenuItem("Seblak special", 15000);
-        MenuItem Seblak hemat = new MenuItem("Seblak hemat", 12000);
+        MenuItem SeblakSpecial = new MenuItem("Seblak Special", 15000);
+        MenuItem SeblakHemat = new MenuItem("Seblak Hemat", 12000);
         MenuItem esTeh = new MenuItem("Es Teh", 5000);
         MenuItem Joshua = new MenuItem("Joshua", 8000);
 
@@ -18,8 +18,8 @@ public class MainApp {
         char ulang;
         do {
             System.out.println("\nPilih menu:");
-            System.out.println("1. Seblak special - Rp15.000");
-            System.out.println("2. Seblak hemat - Rp12.000");
+            System.out.println("1. Seblak Special - Rp15.000");
+            System.out.println("2. Seblak Hemat - Rp12.000");
             System.out.println("3. Es Teh - Rp5.000");
             System.out.println("4. Joshua - Rp8.000");
             System.out.print("Pilih (1-4): ");
@@ -29,10 +29,10 @@ public class MainApp {
             int jumlah = input.nextInt();
 
             switch (pilih) {
-                case 1 -> order.tambahPesanan(new OrderItem(nasiGoreng, jumlah));
-                case 2 -> order.tambahPesanan(new OrderItem(mieAyam, jumlah));
+                case 1 -> order.tambahPesanan(new OrderItem(SeblakSpecial, jumlah));
+                case 2 -> order.tambahPesanan(new OrderItem(SeblakHemat, jumlah));
                 case 3 -> order.tambahPesanan(new OrderItem(esTeh, jumlah));
-                case 4 -> order.tambahPesanan(new OrderItem(kopi, jumlah));
+                case 4 -> order.tambahPesanan(new OrderItem(Joshua, jumlah));
                 default -> System.out.println("Menu tidak tersedia!");
             }
 
